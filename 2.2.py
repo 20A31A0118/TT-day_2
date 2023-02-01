e9428f1e8ef9f39fe5d1ff30e15d35ddf1ea3151
20A31A0118 <124040423+20A31A0118@users.noreply.github.com>
2023-02-01 20:00:14 +0530

    Add files via upload

---
+++ 2.2.py
@@ -0,0 +1,70 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:/Users/91961/Desktop/keerthana/Day_3/2.py =============
+Traceback (most recent call last):
+  File "C:/Users/91961/Desktop/keerthana/Day_3/2.py", line 2, in <module>
+    print(nmu)
+NameError: name 'nmu' is not defined
+>>> 
+============= RESTART: C:/Users/91961/Desktop/keerthana/Day_3/2.py =============
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+>>> 
+============= RESTART: C:/Users/91961/Desktop/keerthana/Day_3/2.py =============
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+first ele in list  1
+>>> 
+============= RESTART: C:/Users/91961/Desktop/keerthana/Day_3/2.py =============
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+first ele in list  1
+[3, 4, 5]
+>>> 
+============= RESTART: C:/Users/91961/Desktop/keerthana/Day_3/2.py =============
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+first ele in list  1
+[3, 4, 5]
+[1, 3, 5, 7, 9]
+>>> 
+============= RESTART: C:/Users/91961/Desktop/keerthana/Day_3/2.py =============
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+first ele in list  1
+[3, 4, 5]
+[1, 3, 5, 7, 9]
+3
+>>> 
+============= RESTART: C:/Users/91961/Desktop/keerthana/Day_3/2.py =============
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+first ele in list  1
+[3, 4, 5]
+[1, 3, 5, 7, 9]
+3
+6
+>>> 
+============= RESTART: C:/Users/91961/Desktop/keerthana/Day_3/2.py =============
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+first ele in list  1
+[3, 4, 5]
+[1, 3, 5, 7, 9]
+3
+6
+[2, 3]
+>>> 
+============= RESTART: C:/Users/91961/Desktop/keerthana/Day_3/2.py =============
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+first ele in list  1
+[3, 4, 5]
+[1, 3, 5, 7, 9]
+3
+6
+[2, 5, 8]
+>>> 
+============= RESTART: C:/Users/91961/Desktop/keerthana/Day_3/2.py =============
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+first ele in list  1
+[3, 4, 5]
+[1, 3, 5, 7, 9]
+3
+6
+[2, 5, 8]
+[1, 2, 5, 6, 7, 8, 9, 10]
+>>> 
