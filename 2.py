8482ea37f1a8865320cc5fd169d67cba7bee7661
20A31A0118 <124040423+20A31A0118@users.noreply.github.com>
2023-02-03 04:47:27 +0530

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,68 +1,69 @@
 Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> 
-========= RESTART: C:/Users/91961/Desktop/keerthana/Day_2/Day.txt/3.py =========
-HELLO WORLD
->>> 
-========= RESTART: C:/Users/91961/Desktop/keerthana/Day_2/Day.txt/3.py =========
->>> 
->>> 
->>> 
-========== RESTART: C:\Users\91961\Desktop\keerthana\Day_2\Day.py\1.py =========
-
-Traceback (most recent call last):
-  File "C:\Users\91961\Desktop\keerthana\Day_2\Day.py\1.py", line 1, in <module>
-    days=int(input())
-ValueError: invalid literal for int() with base 10: ''
->>> 
-========== RESTART: C:\Users\91961\Desktop\keerthana\Day_2\Day.py\1.py =========
-
-Traceback (most recent call last):
-  File "C:\Users\91961\Desktop\keerthana\Day_2\Day.py\1.py", line 1, in <module>
-    days=int(input())
-ValueError: invalid literal for int() with base 10: ''
->>> 
-========== RESTART: C:\Users\91961\Desktop\keerthana\Day_2\Day.py\1.py =========
-
-========== RESTART: C:\Users\91961\Desktop\keerthana\Day_2\Day.py\1.py =========
-34
-12
-67
-5768
-remaining days: 50
-remaining calls: 2988
-remaining msgs: 33
-limit exceed
->>> 
-========== RESTART: C:\Users\91961\Desktop\keerthana\Day_2\Day.py\1.py =========
-85
-your plan expired
->>> 82
-82
->>> 88
-88
->>> 
-========== RESTART: C:\Users\91961\Desktop\keerthana\Day_2\Day.py\1.py =========
-82
-2789
-39
-1.5
-remaining days: 2
-remaining calls: 211
-remaining msgs: 61
-remaining data: 0.5
->>> 
-========== RESTART: C:\Users\91961\Desktop\keerthana\Day_2\Day.py\1.py =========
+========== RESTART: C:/Users/91961/Desktop/keerthana/Day_2/Day.py/1.py =========
 -10
-20
-3000
-2.1
-remaining days: 94
-remaining calls: 2980
-msg failed
-limit exceed
+-9
+-8
+-7
+-6
+-5
+-4
+-3
+-2
+-1
 >>> 
-========== RESTART: C:\Users\91961\Desktop\keerthana\Day_2\Day.py\1.py =========
+========== RESTART: C:/Users/91961/Desktop/keerthana/Day_2/Day.py/1.py =========
+-10
+-8
+-6
+-4
+-2
+>>> 
+========== RESTART: C:/Users/91961/Desktop/keerthana/Day_2/Day.py/1.py =========
+-10
+-8
+-6
+-4
+-2
+>>> 
+========== RESTART: C:/Users/91961/Desktop/keerthana/Day_2/Day.py/1.py =========
+-10
+-8
+-6
+-4
+-2
+>>> 
+========== RESTART: C:/Users/91961/Desktop/keerthana/Day_2/Day.py/1.py =========
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+>>> 
+========== RESTART: C:/Users/91961/Desktop/keerthana/Day_2/Day.py/1.py =========
+>>> 
+========== RESTART: C:/Users/91961/Desktop/keerthana/Day_2/Day.py/1.py =========
 1 
 
 2 2 
@@ -74,90 +75,57 @@ limit exceed
 5 5 5 5 5 
 
 >>> 
-========== RESTART: C:\Users\91961\Desktop\keerthana\Day_2\Day.py\1.py =========
+========== RESTART: C:/Users/91961/Desktop/keerthana/Day_2/Day.py/1.py =========
 A 
 
-C C 
+B B 
 
-E E E 
+C C C 
 
-G G G G 
+D D D D 
 
-I I I I I 
+E E E E E 
 
-K K K K K K 
+F F F F F F 
 
-M M M M M M M 
+G G G G G G G 
 
-O O O O O O O O 
+H H H H H H H H 
 
-Q Q Q Q Q Q Q Q Q 
+I I I I I I I I I 
 
-S S S S S S S S S S 
+J J J J J J J J J J 
 
-U U U U U U U U U U U 
+K K K K K K K K K K K 
 
-W W W W W W W W W W W W 
+L L L L L L L L L L L L 
 
-Y Y Y Y Y Y Y Y Y Y Y Y Y 
+M M M M M M M M M M M M M 
 
->>> 
-========== RESTART: C:\Users\91961\Desktop\keerthana\Day_2\Day.py\1.py =========
-1 * 10= 10 
+N N N N N N N N N N N N N N 
 
-2 * 10= 20 
+O O O O O O O O O O O O O O O 
 
-3 * 10= 30 
+P P P P P P P P P P P P P P P P 
 
-4 * 10= 40 
+Q Q Q Q Q Q Q Q Q Q Q Q Q Q Q Q Q 
 
-5 * 10= 50 
+R R R R R R R R R R R R R R R R R R 
 
-6 * 10= 60 
+S S S S S S S S S S S S S S S S S S S 
 
-7 * 10= 70 
+T T T T T T T T T T T T T T T T T T T T 
 
-8 * 10= 80 
+U U U U U U U U U U U U U U U U U U U U U 
 
-9 * 10= 90 
+V V V V V V V V V V V V V V V V V V V V V V 
 
-10 * 10= 100 
+W W W W W W W W W W W W W W W W W W W W W W W 
 
->>> 
-========== RESTART: C:\Users\91961\Desktop\keerthana\Day_2\Day.py\1.py =========
-1 *5= 5
-2 *5= 10
-3 *5= 15
-4 *5= 20
-5 *5= 25
-6 *5= 30
-7 *5= 35
-8 *5= 40
-9 *5= 45
-10 *5= 50
->>> 
-========== RESTART: C:\Users\91961\Desktop\keerthana\Day_2\Day.py\1.py =========
-1 
+X X X X X X X X X X X X X X X X X X X X X X X X 
 
-1 2 
+Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y Y 
 
-1 2 3 
+Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z 
 
-1 2 3 4 
-
-1 2 3 4 5 
-
-1 2 3 4 5 6 
-
-1 2 3 4 5 6 7 
-
-1 2 3 4 5 6 7 8 
-
-1 2 3 4 5 6 7 8 9 
-
-1 2 3 4 5 6 7 8 9 10 
-
->>> 
-========== RESTART: C:\Users\91961\Desktop\keerthana\Day_2\Day.py\1.py =========
-7.0710678118654755
 >>> 
---
+++ 3.py
@@ -0,0 +1,12 @@
+'''print("HELLO WORLD")'''
+
+
+def check():
+    if c<50:
+        print('less than 50')
+        b=b*2
+        a=a+10
+    else:
+        print('>=50')
+        a=a*2
+        b=b+10
